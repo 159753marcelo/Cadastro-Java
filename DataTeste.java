10d9cdef0c0cd846d356db3d3532c1e6a9443640
Marcelo Costa <marcel0.c7sta@gmail.com>
2024-03-09 19:29:02 -0300

    bora

---
+++ DataTeste.java
@@ -0,0 +1,21 @@
+public class DataTeste {
+    public static void main(String[] args) { 
+
+         Data d1 = new Data();
+
+         d1.dia = 06;
+         d1.mes = 03;
+         d1.ano = 1215;
+
+       System.out.printf( "%d/%d/%d\n" , d1.dia, d1.mes,  d1.ano );
+
+        var d2 = new Data();
+        d2.dia = 29;
+        d2.mes = 12;
+        d2.ano = 1967;
+        
+        System.out.printf("%d/%d/%d" , d2.dia, d2.mes, d2.ano);
+        
+        }
+    
+}
---
+++ ProdutoTeste.java
@@ -0,0 +1,22 @@
+public class ProdutoTeste {
+    public static void main (String[] args){
+        Produto p1 = new Produto();
+        p1.nome = " Notebook";
+        p1.preco = 456.36;
+        p1.desconto = 0.25;
+        var p2 = new Produto();
+        p2.nome = "Lápis";
+        p2.preco = 5.25;
+        p2.desconto = 0.29;
+
+
+        System.out.print(p1.nome);
+        System.out.print(p2.nome);
+
+        double precofinal1 = p1.preco *(1-p1.desconto );
+        double precofinal2 = p1.preco *(1-p2.desconto );
+        double mediaCarrinho = (precofinal1+precofinal2) / 2;
+
+        System.out.printf("Média do carrinho = R$%.2f.", mediaCarrinho );
+    }
+}
